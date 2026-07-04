e176e9a59897730e129cb477fda1af3f82bea766
shubhu1026 <shubhu.p2000@gmail.com>
2026-07-03 20:24:55 -0400

    cv download update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,11 @@ export const AboutSection = () => {
                             Get in touch
                         </a>
 
-                        <a href="resume/Shubham_Patel_Resume.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-200">
+                        <a 
+                            href="resume/Shubham_Patel_Resume.pdf" 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-200">
                             {" "}
                             Download CV
                         </a>
